8fb89a334b66f537ba9fb29a13afc7aa198f065b
Dalton Newport <dalton.e.newport-1@ou.edu>
2019-11-05 15:33:15 -0600

    Made an empty class for a test

---
+++ BigProject/FirstAttempt/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAttempt
+{
+    class Question
+    {
+
+        public string Body { get; set; }
+
+    }
+}
